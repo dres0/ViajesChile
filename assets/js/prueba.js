12665cfe2a4b6189f9447c44c9657f4ef8239a0c
dres0 <dres0@DESKTOP-RN3G9FF.localdomain>
2022-03-19 02:28:37 -0300

    first commit

---
+++ assets/js/prueba.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+            window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$("#enviarContacto").click(function(){
+    alert("Botón no habilitado...");
+});
